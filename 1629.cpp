a9f021d6e4d3dac523ffda4b104ac47ee50c8bd2
Duc Tai Dinh <ductaidt05@gmail.com>
2025-04-18 17:41:31 +0700

    add 1629 2183

---
+++ 1629.cpp
@@ -0,0 +1,35 @@
+// Duc Tai Dinh [cieldt] - 23122013, AI23 @ HCMUS-VNUHCM
+// 16:17:11, 18/04/2025
+// https://cses.fi/problemset/task/1629
+// --------------------------------
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+    int n; cin >> n;
+    vector<pair<int, int>> movies(n);
+    for (int i = 0; i < n; i++){
+        cin >> movies[i].first >> movies[i].second;
+    }
+    sort(movies.begin(), movies.end(), [](pair<int, int> a, pair<int, int> b){
+        return a.second < b.second;
+    });
+    int end = 0, res = 0;
+    for (auto it : movies){
+        if (it.first >= end){
+            res++;
+            end = it.second;
+        }
+    }
+    cout << res;
+}
+
+int main() {
+    solve();
+    return 0;
+}
---
+++ 2183.cpp
@@ -0,0 +1,35 @@
+// Duc Tai Dinh [cieldt] - 23122013, AI23 @ HCMUS-VNUHCM
+// 16:53:55, 18/04/2025
+// https://cses.fi/problemset/task/2183
+// --------------------------------
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define ll long long
+using namespace std;
+
+void solve(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    ll n; cin >> n;
+    ll res = 1;
+    vector<ll> num(n);
+    for (ll i = 0; i < n; i++) cin >> num[i];
+    sort(num.begin(), num.end(), [](ll a, ll b){
+        return a < b;
+    });
+    // for (ll i = 0; i < n; i++) cout << num[i] << " "; cout << '\n';
+    for (ll i = 0; i < n; i++){
+        if (num[i] <= res){
+            res += num[i];
+        } else break;
+    }
+    cout << res;
+}
+
+int main() {
+    solve();
+    return 0;
+}
