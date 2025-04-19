19e81bc04e0db56ac165e1b5723d6e186f65a34d
Duc Tai Dinh <ductaidt05@gmail.com>
2025-04-19 10:34:54 +0700

    add 1163 2162

---
+++ 1163.cpp
@@ -0,0 +1,38 @@
+// Duc Tai Dinh [cieldt] - 23122013, AI23 @ HCMUS-VNUHCM
+// 07:45:29, 19/04/2025
+// https://cses.fi/problemset/task/1163
+// --------------------------------
+#include <iostream>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+void solve(){
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int x, n; cin >> x >> n;
+    set<int> light{0, x};
+    multiset<int> dist{x};
+    int temp;
+    for (int i = 0; i < n; i++){
+        cin >> temp;
+        auto it = light.upper_bound(temp); //*it > temp
+        auto it2 = it; it2--;
+        dist.erase(dist.find(*it - *it2));
+        dist.insert(*it - temp);
+        dist.insert(temp - *it2);
+        light.insert(temp);
+
+        auto res = dist.end();
+        cout << *(--res) << " ";
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
---
+++ 2162.cpp
@@ -0,0 +1,35 @@
+// Duc Tai Dinh [cieldt] - 23122013, AI23 @ HCMUS-VNUHCM
+// 08:41:16, 19/04/2025
+// https://cses.fi/problemset/task/2162
+// --------------------------------
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void solve(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int n; cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) q.push(i);
+
+    bool remove = false;
+    while (!q.empty()){
+        int ele = q.front();
+        q.pop();
+        if (remove){
+            cout << ele << " ";
+        } else {
+            q.push(ele);
+        }
+        remove = !remove;
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
