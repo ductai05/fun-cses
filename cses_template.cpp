0549d0c8e8833def81eaed0d613de34ca2422ba3
Duc Tai Dinh <139867612+ductai05@users.noreply.github.com>
2025-09-08 20:52:21 +0700

    Create cses_template.cpp

---
+++ cses_template.cpp
@@ -0,0 +1,26 @@
+// Duc Tai Dinh [cieldt] - 23122013, AI23 @ HCMUS-VNUHCM
+// hh:mm:ss, dd/mm/yyyy
+// https://cses.fi/problemset
+// --------------------------------
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+
+typedef long long ll;
+
+using namespace std;
+
+
+void solve(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+}
+
+int main() {
+    solve();
+    return 0;
+}
